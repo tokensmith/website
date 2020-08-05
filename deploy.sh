aebd2ffdefaff1afa149e6ec6f9f58d3ea7565f8
= <tom.s.mackenzie@gmail.com>
2020-08-05 18:34:00 -0500

    8: push before deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 project_id=$1
 
-docker build --build-arg HUGO_ENV="production" -t site .
-
-docker tag site gcr.io/$project_id/site
-
+docker build --build-arg HUGO_ENV="production" -t gcr.io/$project_id/site .
+docker push gcr.io/$project_id/site
 gcloud run deploy site --image gcr.io/$project_id/site --region us-central1 --platform managed
